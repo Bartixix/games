fd6afa6938b08dedf76cf8f585fcd751a4d5ae2d
Bartixix <bartek.zmitrukiewicz@gmail.com>
2024-01-09 23:19:58 +0100

    sync

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const pages = {
+  game: "game",
+  tournament: "tournament",
+};
+
 function setSideWidth() {
   let currentWidth = 0;
   const elements = document.querySelectorAll(".side-menu-element");
@@ -15,18 +20,57 @@ function setSideWidth() {
 async function init(target) {
   switch (target) {
     case "main":
+      await init(pages.tournament);
+      break;
+    case pages.game:
+      await fetch("./html_pages/game.html")
+        .then((res) => res.text())
+        .then((res) => (document.getElementById("content").innerHTML = res));
+      break;
+    case pages.tournament:
       await fetch("./html_pages/tournament.html")
         .then((res) => res.text())
         .then((res) => (document.getElementById("content").innerHTML = res));
 
-      document.getElementById("uploadFile").addEventListener("change", (e) => {
-        let fr = new FileReader();
-        fr.onload=function(){
-          console.log(fr.result)
-        };
-        fr.readAsText(e.target.files[0]);
-      });
+      initTournament();
       break;
+  }
+}
+
+function initTournament() {
+  document.getElementById("uploadFile").addEventListener("change", (e) => {
+    let fr = new FileReader();
+    fr.onload = function () {
+      console.log(fr.result);
+    };
+    fr.readAsText(e.target.files[0]);
+  });
+
+  document.getElementById("create").addEventListener("click", (e) => {
+    initCreate(4);
+  });
+}
+
+function initCreate(count) {
+  for (let i = 0; i < count; i++) {
+    const parrent = document.getElementById("players");
+
+    const element = document.createElement("div");
+    const input = document.createElement("input");
+    const remvoeBtn = document.createElement("button");
+
+    element.className = "user";
+    element.id = `user${i}`;
+    input.className = "user-element user-input";
+    input.id = `user-input${i}`;
+    input.placeholder = `Player ${i + 1}`;
+    input.required = true
+    remvoeBtn.className = "user-element user-btn";
+    remvoeBtn.id = `user-btn${i}`;
+
+    element.appendChild(input);
+    element.appendChild(remvoeBtn);
+    parrent.appendChild(element);
   }
 }
 
